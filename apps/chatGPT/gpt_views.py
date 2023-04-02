090d2529e22ecc859c561d95904711a972c597e7
yoga14s <2640408641@qq.com>
2023-04-02 21:17:12 +0800

    tmp

--- apps/chatGPT/gpt_views.py
+++ apps/chatGPT/gpt_views.py
@@ -23,7 +23,71 @@ async def query_gpt(form: QueryGPTForm):
     # restart_sequence = "Q: "
     try:
         response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=msg,
+            temperature=0.9,
+            max_tokens=200,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        # print(start_sequence, response["choices"][0]["text"].strip())
+        print(response)
+        return {
+            "status": "success",
+            "msg": response["choices"][0]["text"].strip(),
+            "total_tokens": response['usage']['total_tokens']
+        }
+    except Exception as exc:
+        print(exc)
+        return {"status": "fail", "msg": "服务器内部出错！"}
+
+@router.post("/query_gpt_davinci/")
+async def query_gpt(form: QueryGPTForm):
+    msg = form.msgContent
+    if not msg:
+        return {"status": "fail", "msg": "请输入非空信息！"}
+
+    print(form.msgContent)
+
+    openai.api_key = settings.OPEN_API_KEY
+    # start_sequence = "A: "
+    # restart_sequence = "Q: "
+    try:
+        response = openai.Completion.create(
+            # model="text-curie-001",
+            model="text-davinci-003",
+            prompt=msg,
+            temperature=0.9,
+            max_tokens=200,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        # print(start_sequence, response["choices"][0]["text"].strip())
+        print(response)
+        return {
+            "status": "success",
+            "msg": response["choices"][0]["text"].strip(),
+            "total_tokens": response['usage']['total_tokens']
+        }
+    except Exception as exc:
+        print(exc)
+        return {"status": "fail", "msg": "服务器内部出错！"}
+
+@router.post("/query_gpt_curie/")
+async def query_gpt(form: QueryGPTForm):
+    msg = form.msgContent
+    if not msg:
+        return {"status": "fail", "msg": "请输入非空信息！"}
+
+    print(form.msgContent)
+
+    openai.api_key = settings.OPEN_API_KEY
+    # start_sequence = "A: "
+    # restart_sequence = "Q: "
+    try:
+        response = openai.Completion.create(
             model="text-curie-001",
+            # model="text-davinci-003",
             prompt=msg,
             temperature=0.9,
             max_tokens=200,
