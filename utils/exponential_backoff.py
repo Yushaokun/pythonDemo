0d5086f92197d642c9fca00f05effc165783059b
Yushaokun <2640408641@qq.com>
2023-04-23 15:38:26 +0800

    tmp

--- utils/exponential_backoff.py
+++ utils/exponential_backoff.py
@@ -11,7 +11,7 @@ from tenacity import (
 # 方法一，指数退避算法请求GPT，防止遇到并发瓶颈时报错
 @retry(wait=wait_random_exponential(min=1, max=120), stop=stop_after_attempt(6))
 def completion_with_retry(**kwargs):
-    return openai.Completion.create(**kwargs)
+    return openai.ChatCompletion.create(**kwargs)
 
 
 import backoff
@@ -19,5 +19,5 @@ import backoff
 # 方法二，指数退避算法请求GPT，防止遇到并发瓶颈时报错
 @backoff.on_exception(backoff.expo, openai.error.RateLimitError, max_time=120)
 def completions_with_backoff(**kwargs):
-    return openai.Completion.create(**kwargs)
+    return openai.ChatCompletion.create(**kwargs)
 
