a39de95ed9be25124d4861e642e26559f47e02c9
Yushaokun <2640408641@qq.com>
2023-05-21 09:55:36 +0800

    tmp

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from starlette.staticfiles import StaticFiles
 
 from apps.router import router
 from scheduler.apscheduler_jobs import run_scheduler
+from settings import ENABLE_SCHEDULER
 
 app = fastapi.FastAPI()
 app.include_router(router)
@@ -15,7 +16,8 @@ app.include_router(router)
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
 if __name__ == '__main__':
-    run_scheduler()
+    if ENABLE_SCHEDULER:
+        run_scheduler()
 
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 8010
     workers = int(sys.argv[2]) if len(sys.argv) > 2 else 1
